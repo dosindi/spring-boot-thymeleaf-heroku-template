db901066a799c14888f4fe02cee6dac782f3e5c9
cansoftinc <dunco20ny@gmail.com>
2018-02-12 09:58:43 -0800

    Update SecurityConfig.java

--- src/main/java/com/chrisbaileydeveloper/myapp/config/SecurityConfig.java
+++ src/main/java/com/chrisbaileydeveloper/myapp/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-	            .antMatchers("/").antMatchers("/products/**").anonymous().permitAll()		    
+	            .antMatchers("/").antMatchers("/api/products/**").anonymous().permitAll()		    
 	            .and()
             .formLogin()
 	            .loginPage("/signin")
